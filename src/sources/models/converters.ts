ef6d801af8ca4356069a2a3ce47f3c4ccad50c63
galta <48960890+galta95@users.noreply.github.com>
2021-01-11 16:50:36 +0200

    feat(external-sources):  added helpers models

---
+++ src/sources/models/converters.ts
@@ -0,0 +1,42 @@
+import { Geometry } from 'geojson';
+import { parse } from 'wellknown';
+import { GeomertyParseError } from './errors';
+
+interface IConverter {
+  (geometry: unknown): Promise<Geometry>;
+}
+
+export type Converters = Record<GeoTypes, IConverter>;
+
+function isGeoJson(geometry: unknown): geometry is Geometry {
+  const geoJsonTypes = ['Polygon', 'MultiPoint', 'LineString', 'MultiLineString', 'MultiPolygon', 'GeometryCollection'];
+  return geoJsonTypes.includes((geometry as Geometry).type);
+}
+
+export async function wktConverter(geometry: unknown): Promise<Geometry> {
+  if (!(typeof geometry === 'string')) {
+    throw new GeomertyParseError(`Cannot convert geometry to geoJson geometry`);
+  }
+  const converted = parse(geometry);
+  if (converted === null) {
+    throw new GeomertyParseError(`Cannot convert geometry to geoJson geometry`);
+  }
+  return Promise.resolve(converted);
+}
+
+export async function geoJsonConverter(geometry: unknown): Promise<Geometry> {
+  if (!isGeoJson(geometry)) {
+    throw new GeomertyParseError(`Cannot convert geometry to geoJson geometry`);
+  }
+  return Promise.resolve(geometry);
+}
+
+export enum GeoTypes {
+  WKT = 'wkt',
+  GEO_JSON = 'geojson',
+}
+
+export const converters: Converters = {
+  [GeoTypes.WKT]: wktConverter,
+  [GeoTypes.GEO_JSON]: geoJsonConverter,
+};
