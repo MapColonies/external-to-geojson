b428a92575d4196bf50b784e69e7aabd44e20355
galta <48960890+galta95@users.noreply.github.com>
2021-01-18 13:51:25 +0200

    feat(external-sources): remove logger.log from controller

--- src/sources/controllers/sources.ts
+++ src/sources/controllers/sources.ts
@@ -3,8 +3,6 @@ import httpStatus from 'http-status-codes';
 import { Feature } from 'geojson';
 import { injectable, inject } from 'tsyringe';
 import { HttpError } from '@map-colonies/error-express-handler';
-import { Services } from '../../common/constants';
-import { ILogger } from '../../common/interfaces';
 import { SourcesManager, IExternalData } from '../models/sourcesManager';
 import { GeomertyParseError, GeometryNotFoundError, SchemaNotFoundError } from '../models/errors';
 
@@ -17,7 +15,7 @@ type SourcesHandler = RequestHandler<SchemaParams, Feature, IExternalData>;
 
 @injectable()
 export class SourcesController {
-  public constructor(@inject(Services.LOGGER) private readonly logger: ILogger, @inject(SourcesManager) private readonly manager: SourcesManager) {}
+  public constructor(@inject(SourcesManager) private readonly manager: SourcesManager) {}
 
   public convertSource: SourcesHandler = async (req, res, next) => {
     const sourceName = req.params['external_source_name'];
@@ -34,7 +32,6 @@ export class SourcesController {
       } else if (e instanceof GeometryNotFoundError) {
         error.statusCode = httpStatus.UNPROCESSABLE_ENTITY;
       }
-      this.logger.log('error', error.message, e);
       return next(error);
     }
     return res.status(httpStatus.OK).json(geoJson);
